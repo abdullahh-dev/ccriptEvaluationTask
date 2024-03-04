a9c1cddf9da9e2b7b3273df69f474f0e5c344c18
Muhammad Abdullah <102689133+abdullahh-dev@users.noreply.github.com>
2024-03-04 13:05:45 +0500

    Update page.js

--- frontend/todo/src/app/page.js
+++ frontend/todo/src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
   const addTask = () => {
     if (!task.trim()) {
       alert("Please Enter a Task");
+      setTask('');
       return;
     }
     let taskName = task;
